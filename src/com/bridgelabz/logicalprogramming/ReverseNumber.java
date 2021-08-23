39f6333498abc18126bee0f6f023aa3075c079ec
Akash-gowda-hr <akashhr2040@gmail.com>
2021-08-23 22:27:36 +0530

    ADDED:coupon number programme

--- src/com/bridgelabz/logicalprogramming/ReverseNumber.java
+++ src/com/bridgelabz/logicalprogramming/ReverseNumber.java
@@ -1,5 +1,21 @@
 package com.bridgelabz.logicalprogramming;
+import java.util.*;
 
 public class ReverseNumber {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int remainder, reversedNumber = 0;
+		System.out.println("Enter the number to be reversed:");
+		int numberToReverse = sc.nextInt();
+		sc.close();
+
+		while (numberToReverse > 0) {
+			remainder = numberToReverse % 10;
+			reversedNumber = (reversedNumber * 10) + remainder;
+			numberToReverse = numberToReverse / 10;
+		}
+
+		System.out.println("The reversed Number is: " + reversedNumber);
+	}
 
 }
